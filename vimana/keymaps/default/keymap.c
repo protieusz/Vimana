f2cd11301955e3b4fab6c331dda6a2456587e61e
Protieus <118025702+protieusz@users.noreply.github.com>
2023-04-23 23:34:15 -0700

    Add scroll lock keycode

--- vimana/keymaps/default/keymap.c
+++ vimana/keymaps/default/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_Q, KC_W, KC_E, KC_R, KC_T,          LT(3,KC_ESC), KC_LALT,         KC_Y, KC_U, KC_I, KC_O, KC_P,
                 KC_A, KC_S, KC_D, KC_F, KC_G,  KC_LCTL,                KC_ENT,  KC_H, KC_J, KC_K, KC_L, KC_SCLN,
         LSFT_T(KC_Z), KC_X, KC_C, KC_V, KC_B,          KC_LGUI, KC_MS_BTN2,     KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH),
-                                       LT(1,KC_BSPC),  KC_MS_BTN1, MO(2),    KC_SPC
+                                       LT(1,KC_BSPC),  KC_MS_BTN1, SCRL_MO,    LT(2,KC_SPC)
     ),
 
     [1] = LAYOUT(
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     ),
     [2] = LAYOUT(
-        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,            _______, _______,           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,            _______, SCRL_TO,           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
         _______, _______, _______, _______, _______, _______,                 _______,   _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,         _______, _______,           _______, _______, _______, _______, _______,
                                               _______,       _______, _______,     _______
